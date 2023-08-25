79664c8abd495641d96aad34253d72ea93d63cad
Saral03 <saralhanda13@gmail.com>
2023-08-25 15:50:41 +0530

    add necessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>,
+  },
+  {
+    path: "/products/:id",
+    element: <div>Category</div>,
+  },
+  {
+    path: "/product/:id",
+    element: <div>Product</div>,
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <RouterProvider router={router} />
   );
 }
 
